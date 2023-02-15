1307c9f4fe06483d1c0f511a712585ca96246beb
Eduard T <eduard.t@wavemyth.com>
2023-02-15 14:53:42 +0200

    Updated introduction page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Starter JS</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/Zoli-88/form',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'Nextra Docs Template',
   },
